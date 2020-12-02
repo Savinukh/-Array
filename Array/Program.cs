df8e6ad1f570677501226c2df06d1981e36ca3c8
Left_Tapochek <serregasavinykh15@gmail.com>
2020-12-02 19:02:20 +0500

    Разбиение двумерного массива на методы

--- Array/Program.cs
+++ Array/Program.cs
@@ -4,12 +4,26 @@ namespace Array
 {
     class Program
     {
-        static void Main(string[] args)
+        static int height = 0;
+        static int width = 0;
+        static void Main()
         {
-            Console.Write("Введите кол-во цифр в строку:\t");
-            int height = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("--------------------------------------------");
+            Console.ResetColor();
+            Console.Write("\nВведите кол-во цифр в строку:\t");
+            height = int.Parse(Console.ReadLine());
             Console.Write("Введите кол-во цифр в столбец:\t");
-            int width = int.Parse(Console.ReadLine());
+            width = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            RandommyArray();
+           
+
+
+            
+        }
+        static void RandommyArray()
+        {
             int[,] myArray = new int[height, width];
             Random ran = new Random();
             for (int y = 0; y < myArray.GetLength(0); y++)
@@ -23,12 +37,17 @@ namespace Array
             {
                 for (int x = 0; x < myArray.GetLength(1); x++)
                 {
-                    Console.Write(myArray[y,x] + " ");
+                    Console.Write(myArray[y, x] + " ");
                 }
                 Console.WriteLine();
             }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n--------------------------------------------");
+            Console.ResetColor();
 
             Console.ReadKey();
         }
+        
+
     }
 }
