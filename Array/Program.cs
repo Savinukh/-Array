e43130b43c4dcb076f7be2976f71680680f043f4
Left_Tapochek <serregasavinykh15@gmail.com>
2020-12-02 18:05:11 +0500

    Создание двумерного массива

--- Array/Program.cs
+++ Array/Program.cs
@@ -6,7 +6,19 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           int[,] myArray = new int[5, 6];
+            int height = myArray.GetLength(0);
+            int width = myArray.GetLength(1);
+            for (int y = 0;y < height; y++)
+            {
+                for (int x = 0;x< width; x++)
+                {
+                    Console.Write(myArray [y,x] + " ");
+                }
+                Console.WriteLine();
+            }
+                         
+        Console.ReadKey();
         }
     }
 }
